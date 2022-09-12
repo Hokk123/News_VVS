4226e77ddb2d2c67294047ec612c8597ef0dc42b
Hokk123 <vladimir_vs@list.ru>
2022-09-12 17:01:05 +0300

    D7

---
+++ News_VVS/celery.py
@@ -0,0 +1,23 @@
+import os
+from celery import Celery
+from celery.schedules import crontab
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'News_VVS.settings')
+
+app = Celery('News_VVS')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.conf.beat_schedule = {
+    # Executes every Monday morning at 8:00
+    'add-weekly-mailings': {
+        'task': 'news.tasks.weekly_notify',
+        'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
+    },
+    # Executes every Day, every 6 hours
+    'parsing-for-add-news': {
+        'task': 'news.tasks.daily_parsing',
+        'schedule': crontab(hour='*/6', minute=0)
+    }
+}
+
+app.autodiscover_tasks()
---
+++ app.py
@@ -0,0 +1,7 @@
+import redis
+
+red = redis.Redis(
+    host='redis-12339.c1.asia-northeast1-1.gce.cloud.redislabs.com',
+    port=12339,
+    password='LVAD9jLOklwBzBHXUJWib8PAoo4fM0vh'
+)
